5adb3bd5f17837f42af24db4f8d8077c246874be
X. Wang <xuyi@tetra.nl>
2024-02-11 22:18:49 +0100

    save

--- src/jx/classfile/datatypes/BCBasicDatatype.java
+++ src/jx/classfile/datatypes/BCBasicDatatype.java
@@ -25,18 +25,18 @@ abstract public class BCBasicDatatype {
 
   // basic datatypes 
   // according to Bytecode instructions (e.g. iaload ff.)
-  public static final int INT = 0;  
-  public static final int LONG = 1; 
-  public static final int FLOAT = 2; 
+  public static final int INT   = 0;  
+  public static final int LONG  = 1; 
+  public static final int FLOAT  = 2; 
   public static final int DOUBLE = 3;
   public static final int REFERENCE = 4; 
-  public static final int BYTE = 5; 
-  public static final int CHAR = 6; 
+  public static final int BYTE  = 5; 
+  public static final int CHAR  = 6; 
   public static final int SHORT = 7; 
 
   public static final int BOOLEAN = 8; 
   public static final int RETURN_ADDRESS = 9;  // possibly not necessary 
-  public static final int VOID = 10; 
+  public static final int VOID  = 10; 
 
   public static final int UNKNOWN_TYPE = -1; 
   // have to adapt some methods of this class before using the next
